2057559078cce806a4d678c72fb7c242d28978a7
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-10 15:43:47 -0700

    Fooling around with animations

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,3 +1,9 @@
+TweenMax.from(".header", 1.5, {opacity:0, scale:0, ease:Bounce.easeOut});
+
+// Makes my articles flip over at opening 
+TweenMax.staggerFrom(".article", 0.5, {rotation:180, scale:2, delay:0.25}, 0.3) 
+
+
 // Setting up my toggleMenu function to toggle the "menu--open" class on my menu reference. 
 const toggleMenu = () => { 
   menu.classList.toggle('menu--open'); 
