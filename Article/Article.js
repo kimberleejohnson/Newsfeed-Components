0351a4e13df53a740be1a272fe03ae3380cc88eb
Kimberlee Johnson <kimee.johnson@gmail.com>
2019-04-10 14:31:56 -0700

    Expanded article toggle method

--- Article/Article.js
+++ Article/Article.js
@@ -1,20 +1,23 @@
 // Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
 
 class Article {
-  constructor(domElement) {
+  constructor(article) {
     // assign this.domElement to the passed in domElement
-    this.domElement;
+    this.article = article;
     // create a reference to the ".expandButton" class. 
-    this.expandButton;
+    this.expandButton = this.article.querySelector('.expandButton');
     // Using your expandButton reference, update the text on your expandButton to say "expand"
+    this.expandButton.innerHTML = "Expand"; 
     
     // Set a click handler on the expandButton reference, calling the expandArticle method.
-    
+    this.expandButton.addEventListener('click', () => this.expandArticle());
   }
 
+  // Methods
+  // Using our reference to the domElement, toggle a class to expand or hide the article.
   expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-
+    this.article.classList.toggle('article-open'); 
+    this.article.classList.toggle('close'); 
   }
 }
 
@@ -26,4 +29,10 @@ class Article {
 
 */
 
-let articles;
+// Selecting all classes named .article and assigning to articles 
+let articles = document.querySelectorAll('.article');
+
+// Creating .forEach chain to iterate over all articles 
+articles.forEach(function(article){
+  return new Article (article); 
+}); 
